889c2542ea46df5ab6578b31b4168d460c90d729
Meghanadh1337 <116261133+Meghanadh1337@users.noreply.github.com>
2024-10-12 15:38:29 +0530

    Update fetchCryptoJob.js
    Added Support for more coins

--- jobs/fetchCryptoJob.js
+++ jobs/fetchCryptoJob.js
@@ -3,14 +3,16 @@ const Cryptocurrency = require('../models/cryptocurrency');
 
 const fetchCryptoData = async () => {
   try {
-    const response = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,matic-network,ethereum&vs_currencies=usd&include_market_cap=true&include_24hr_change=true');
-    const { bitcoin, 'matic-network': matic, ethereum } = response.data;
+    const response = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,matic-network,ethereum,litecoin,ripple&vs_currencies=usd&include_market_cap=true&include_24hr_change=true');
+    const { bitcoin, 'matic-network': matic, ethereum, litecoin, ripple } = response.data;
 
     // Store data in MongoDB
     await Cryptocurrency.insertMany([
       { name: 'Bitcoin', price: bitcoin.usd, marketCap: bitcoin.usd_market_cap, change24h: bitcoin.usd_24h_change },
       { name: 'Matic', price: matic.usd, marketCap: matic.usd_market_cap, change24h: matic.usd_24h_change },
       { name: 'Ethereum', price: ethereum.usd, marketCap: ethereum.usd_market_cap, change24h: ethereum.usd_24h_change },
+      { name: 'Litecoin', price: litecoin.usd, marketCap: litecoin.usd_market_cap, change24h: litecoin.usd_24h_change },
+      { name: 'Ripple', price: ripple.usd, marketCap: ripple.usd_market_cap, change24h: ripple.usd_24h_change },
     ]);
     
     console.log('Crypto data fetched and stored successfully.');
